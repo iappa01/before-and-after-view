3a649df07816554d6cc68aeff17966283627e24f
Tùng Cao Thanh <caothanhtung08082002@gmail.com>
2022-05-12 07:56:18 +0000

    Tung ct/fix after image recycle

--- app/src/main/java/com/mobiai/views/beforeafter/HomeActivity.java
+++ app/src/main/java/com/mobiai/views/beforeafter/HomeActivity.java
@@ -10,7 +10,14 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         BeforeAfter beforeAfter = (BeforeAfter) findViewById(R.id.before_after);
-        beforeAfter.setBeforeImage(BitmapFactory.decodeResource(getResources(), R.drawable.b));
-        beforeAfter.setAfterImage(BitmapFactory.decodeResource(getResources(), R.drawable.ba));
+
+        beforeAfter.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                beforeAfter.setBeforeImage(BitmapFactory.decodeResource(getResources(), R.drawable.b));
+                beforeAfter.setAfterImage(BitmapFactory.decodeResource(getResources(), R.drawable.ba));
+
+            }
+        }, 0);
     }
 }
