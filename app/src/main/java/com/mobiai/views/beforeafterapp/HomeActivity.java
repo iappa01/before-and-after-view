5fbd75f44821b4088df60a13da58bfb141b4f1ed
tung <caothanhtung08082002@gmail.com>
2022-07-09 15:01:45 +0700

    them cach set thumb_height o trong xml

--- app/src/main/java/com/mobiai/views/beforeafterapp/HomeActivity.java
+++ app/src/main/java/com/mobiai/views/beforeafterapp/HomeActivity.java
@@ -15,7 +15,7 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         BeforeAfter beforeAfter = (BeforeAfter) findViewById(R.id.before_after);
-        beforeAfter.setBeforeImage(BitmapFactory.decodeResource(getResources(), R.drawable.b));
-        beforeAfter.setAfterImage(BitmapFactory.decodeResource(getResources(), R.drawable.anh_nang));
+        beforeAfter.setBeforeImage(BitmapFactory.decodeResource(getResources(), R.drawable.anh1));
+        beforeAfter.setAfterImage(BitmapFactory.decodeResource(getResources(), R.drawable.anh2));
     }
 }
